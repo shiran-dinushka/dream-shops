c7a516f3d7da895b1e9365a4cb8c10ab6e8c553a
Shiran Dinushka <shirandinushka9@gmail.com>
2026-05-27 21:19:01 +0530

    remove unnecessary imports

--- src/main/java/com/dailywork/dreamshops/model/Category.java
+++ src/main/java/com/dailywork/dreamshops/model/Category.java
@@ -1,7 +1,7 @@
 package com.dailywork.dreamshops.model;
 
+
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
