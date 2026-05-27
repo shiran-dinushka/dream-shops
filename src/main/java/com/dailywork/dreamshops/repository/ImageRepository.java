1bf0362a9f5986f2a5b33b90b913b9b79eb5e535
Shiran Dinushka <shirandinushka9@gmail.com>
2026-05-27 21:19:53 +0530

    Add findByProductId method

--- src/main/java/com/dailywork/dreamshops/repository/ImageRepository.java
+++ src/main/java/com/dailywork/dreamshops/repository/ImageRepository.java
@@ -3,6 +3,9 @@ package com.dailywork.dreamshops.repository;
 import com.dailywork.dreamshops.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
+    List<Image> findByProductId(Long id);
 
 }
