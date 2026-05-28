e8bbf3e6aaa94e50deed20265268fde300741148
Shiran Dinushka <shirandinushka9@gmail.com>
2026-05-28 19:59:32 +0530

    Add CartRepository and CartItemRepository models

---
+++ src/main/java/com/dailywork/dreamshops/repository/CartItemRepository.java
@@ -0,0 +1,8 @@
+package com.dailywork.dreamshops.repository;
+
+import com.dailywork.dreamshops.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+    void deleteAllByCartId(Long id);
+}
