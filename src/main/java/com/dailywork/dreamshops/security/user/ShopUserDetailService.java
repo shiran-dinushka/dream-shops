e504a1da7869210767b89f75a3a7eaa2765b0e3f
Shiran Dinushka <shirandinushka9@gmail.com>
2026-06-11 21:04:00 +0530

    Add ShopUserDetails and ShopUserDetailService

---
+++ src/main/java/com/dailywork/dreamshops/security/user/ShopUserDetailService.java
@@ -0,0 +1,25 @@
+package com.dailywork.dreamshops.security.user;
+
+import com.dailywork.dreamshops.model.User;
+import com.dailywork.dreamshops.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
+}
